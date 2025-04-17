d64a8d83c6cd67fcd5877de9529124b1323484d7
chitramsst <chitra.msst@gmail.com>
2025-04-17 16:08:38 +0530

    persist app

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,11 @@ import authReducer from "./reducers/authReducer";
 import thunk from "redux-thunk";
 import { persistStore, persistReducer } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage"; 
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
 
 const persistConfig = {
   key: "root",
@@ -11,13 +16,11 @@ const persistConfig = {
 };
 
 
-const persistedReducer = persistReducer(persistConfig, authReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 
 const store = configureStore({
-  reducer: {
-    auth: persistedReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, 
